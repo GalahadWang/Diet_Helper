500ff92f4b743e2ea566c76a82108a154840dad5
Arno <1945374160@qq.com>
2023-10-29 14:58:20 +1100

    跨域问题

--- src/main/java/com/example/diet_helper/filter/CorsFilter.java
+++ src/main/java/com/example/diet_helper/filter/CorsFilter.java
@@ -19,15 +19,6 @@ import java.io.IOException;
 @Order(Const.ORDER_CORS)
 public class CorsFilter extends HttpFilter {
 
-    /**
-     * Filter and enhance HTTP responses by adding CORS headers before passing the request to the next filter in the filter chain.
-     *
-     * @param request  Incoming HTTP Servlet request
-     * @param response Outgoing HTTP Servlet response
-     * @param chain    A chain of filters for handling requests and responses
-     * @throws IOException       I/O error occurs during filtering processing
-     * @throws ServletException servlet-related error occurs during filtering processing
-     */
     @Override
     protected void doFilter(HttpServletRequest request,
                             HttpServletResponse response,
@@ -39,20 +30,11 @@ public class CorsFilter extends HttpFilter {
         chain.doFilter(request, response);
     }
 
-    /**
-     * Adds a CORS header to the HTTP response based on the source of the request and the allowed HTTP method.
-     *
-     * @param request  HTTP Servlet request
-     * @param response HTTP Servlet respond
-     */
     private void addCorsHeader(HttpServletRequest request, HttpServletResponse response) {
-        // set the allowed source to the Origin header value in the request
         response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 
-        // define allowable HTTP methods for CORS requests
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
-        // specifies the allowed header of the CORS request
         response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
     }
 }
